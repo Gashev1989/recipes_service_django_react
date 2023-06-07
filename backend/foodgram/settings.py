084d52110e9779870bf01593829b76a192670c17
Константин Гашев <gashev1989@yandex.ru>
2023-06-08 02:11:12 +0500

    Changed settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,14 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-from pathlib import Path
+# from pathlib import Path
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
