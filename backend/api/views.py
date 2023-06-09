4bb55ee717dcae38a0a7d6ccec612f9c1ac90237
Константин Гашев <gashev1989@yandex.ru>
2023-06-10 03:13:10 +0500

    Flake8 cleaned

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from rest_framework import filters, status
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import (SAFE_METHODS, AllowAny,
                                         IsAuthenticated,
